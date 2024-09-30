19847c858c0870105467ffb1acefcc09ae8b9fb2
Ficus <228fikusik@gmail.com>
2024-09-30 10:37:23 +0500

    da

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _counter = 0;
 
   void _incrementCounter() {
     setState(() {
