6c8782ad1bd279acc5e485d13b8380d9661fb4f8
Ficus <228fikusik@gmail.com>
2024-10-18 14:22:53 +0500

    supoer fix 228

--- lib/pages/upgrades.dart
+++ lib/pages/upgrades.dart
@@ -20,8 +20,8 @@ class UpgradesPage extends StatefulWidget {
 class _UpgradesPageState extends State<UpgradesPage> {
   final TextEditingController _promoController = TextEditingController();
   String _promoMessage = '';
-  String _selectedAction = 'Купить'; // Для отслеживания выбранной кнопки
-  String _selectedMultiplier = '1x'; // Для отслеживания выбранного множителя
+  String _selectedAction = 'Купить';
+  String _selectedMultiplier = '1x';
 
   void _checkPromoCode() {
     if (_promoController.text == "ilovenan") {
@@ -95,7 +95,7 @@ class _UpgradesPageState extends State<UpgradesPage> {
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
         decoration: BoxDecoration(
-          color: isSelected ? Color(0xFF25537C) : Color(0xFF1D466C),
+          color: isSelected ? const Color(0xFF25537C) : const Color(0xFF1D466C),
           borderRadius: BorderRadius.circular(8),
         ),
         child: Text(
@@ -117,7 +117,7 @@ class _UpgradesPageState extends State<UpgradesPage> {
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
         decoration: BoxDecoration(
-          color: isSelected ? Color(0xFF25537C) : Color(0xFF1D466C),
+          color: isSelected ? const Color(0xFF25537C) : const Color(0xFF1D466C),
           borderRadius: BorderRadius.circular(8),
         ),
         child: Text(
@@ -136,91 +136,96 @@ class _UpgradesPageState extends State<UpgradesPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Upgrades'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Top Stats Row using buildMenuCard
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: ListView(
+        children: [
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildMenuCard('Earn per tap', '+${widget.tapStr}', 'assets/image/coin.png', Colors.orange, 120),
+              buildMenuCard('Coins to level up', '${widget.lvlPrice}', 'assets/image/coin.png', Colors.blue, 120),
+              buildMenuCard('Profit per second', '+${widget.pasProf}', 'assets/image/coin.png', Colors.green, 120),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                buildMenuCard('Earn per tap', '+${widget.tapStr}', 'assets/image/coin.png', Colors.orange, 120),
-                buildMenuCard('Coins to level up', '${widget.lvlPrice}', 'assets/image/coin.png', Colors.blue, 120),
-                buildMenuCard('Profit per second', '+${widget.pasProf}', 'assets/image/coin.png', Colors.green, 120),
+                // Promo Code Section
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1D466C),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _promoController,
+                          decoration: const InputDecoration(
+                            hintText: 'Промокод',
+                            hintStyle: TextStyle(color: Colors.white54),
+                            border: InputBorder.none,
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _checkPromoCode,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Text(
+                            _promoMessage.isEmpty ? 'Применить' : _promoMessage,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Buy/Sell and Multipliers Section
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1D466C),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildActionButton('Купить'),
+                      _buildActionButton('Продать'),
+                      _buildMultiplierButton('1x'),
+                      _buildMultiplierButton('10x'),
+                      _buildMultiplierButton('100x'),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Upgrades List внутри Padding и Expanded
+                SizedBox(
+                  height: 400, // Задаем фиксированную высоту, чтобы контент был видим.
+                  child: ListView(
+                    children: [
+                      buildUpgradeCard('Пекарь', '1.61K', 'assets/image/upg1.png', '156.92K'),
+                      buildUpgradeCard('Пекарь-кондитер', '1.61K', 'assets/image/upg2.png', '156.92K'),
+                      buildUpgradeCard('Пекарный аппарат', '1.61K', 'assets/image/upg3.png', '156.92K'),
+                      buildUpgradeCard('Супер пекарь', '1.61K', 'assets/image/upg4.png', '156.92K'),
+                    ],
+                  ),
+                ),
               ],
             ),
-            const SizedBox(height: 16),
-            // Promo Code Section
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1D466C),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _promoController,
-                      decoration: const InputDecoration(
-                        hintText: 'Промокод',
-                        hintStyle: TextStyle(color: Colors.white54),
-                        border: InputBorder.none,
-                      ),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: _checkPromoCode,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Text(
-                        _promoMessage.isEmpty ? 'Применить' : _promoMessage,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            // Buy/Sell and Multipliers Section
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1D466C),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildActionButton('Купить'),
-                  _buildActionButton('Продать'),
-                  _buildMultiplierButton('1x'),
-                  _buildMultiplierButton('10x'),
-                  _buildMultiplierButton('100x'),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            // Upgrades List
-            Expanded(
-              child: ListView(
-                children: [
-                  buildUpgradeCard('Пекарь', '1.61K', 'assets/image/upg1.png', '156.92K'),
-                  buildUpgradeCard('Пекарь-кондитер', '1.61K', 'assets/image/upg2.png', '156.92K'),
-                  buildUpgradeCard('Пекарный аппарат', '1.61K', 'assets/image/upg3.png', '156.92K'),
-                  buildUpgradeCard('Супер пекарь', '1.61K', 'assets/image/upg4.png', '156.92K'),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
